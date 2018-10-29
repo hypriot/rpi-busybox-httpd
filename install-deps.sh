4be36bc972fc957999981d37a30911b481cbac34
Ross Beazley <ross@rossbeazley.co.uk>
2018-10-29 07:41:31 +0100

    Add dockerize dep rsync

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install -y make busybox python-pip
+sudo apt-get install -y make busybox python-pip rsync
 sudo pip install https://github.com/larsks/dockerize/archive/master.zip
